79a9ed0a3b33faa4f981cc466d0d7702cd1e8b8b
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-23 15:56:33 +0530

    useRouter Hook

--- next-practice/pages/about.js
+++ next-practice/pages/about.js
@@ -1,11 +1,21 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+
+// 
 
 
 const About = () => {
+   const router = useRouter()
+   console.log(router)
    return (
       <div>
          <h1>About</h1>
+         <h3>{router.pathname}</h3>
          <Link href="/">Home</Link>
+         <br />
+
+         {/* to push to specific route */}
+         <button onClick={() => router.push('/events')} >Home</button>
       </div>
    )
 }
