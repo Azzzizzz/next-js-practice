787b4c45e85af2fa1278a0400fff2679ef64f382
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-12-04 22:21:19 +0530

    fetch users

--- next-practice/pages/practice/ninjaListing.js
+++ next-practice/pages/practice/ninjaListing.js
@@ -1,10 +1,27 @@
 import Layout2 from "@/components/Layout2";
+import styles from "@/styles/Ninjas.module.css"
 
-const NinjaListing = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const NinjaListing = ({ ninjas }) => {
   return (
     <Layout2>
       <div>
         <h1>Ninja Listing</h1>
+        {ninjas.map(ninja => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </Layout2>
   );
